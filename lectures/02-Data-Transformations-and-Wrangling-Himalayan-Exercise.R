7d2aa52d817200235ae01ea6a447be342241da27
adisarid <adi@sarid-ins.co.il>
2020-12-13 10:21:08 +0200

    started adding exercises

---
+++ lectures/02-Data-Transformations-and-Wrangling-Himalayan-Exercise.R
@@ -0,0 +1,45 @@
+# Himalayan Climbing Expeditions exercise
+
+# Loading libraries -------------------------------------------------------
+
+library(tidyverse)
+
+# Reading the data --------------------------------------------------------
+
+members <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/members.csv')
+expeditions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/expeditions.csv')
+peaks <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/peaks.csv')
+
+# Exercise 1: initial exploration of the data -----------------------------
+
+# Using functions such as glimpse, count, and View, explore the three data sets
+# * Discuss what you see within your group, and answer:
+# * What is the size of each data set? (number of cases = rows and number of variables = columns)
+# * What are the relationships between the data sets? (how are they related to one another)
+# * What variables are shared between the data sets?
+# * Which variable indicates when a peak was first ascended?
+# * How can you find out if and when a specific expedition reached the peak?
+# * How would you find out the role of a specific expedition member, and if he suffered an injury?
+# * How would you find the number of deaths with a specific expedition?
+#      (at this stage don't compute it, just think how to - we will do it later on)
+# * How would you find the number of injuries with a specific expedition?
+#      (at this stage don't compute it, just think how to - we will do it later on)
+
+# Exercise 2: understanding variables and values --------------------------
+
+
+# Exercise 3 --------------------------------------------------------------
+
+
+# Exercise 4 --------------------------------------------------------------
+
+
+# Exercise 5 --------------------------------------------------------------
+
+
+# Exercise 6 --------------------------------------------------------------
+
+
+# Exercise 7 --------------------------------------------------------------
+
+
